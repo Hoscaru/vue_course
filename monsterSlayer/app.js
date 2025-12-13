0f5b9cb6b6963425425662af3d717fd5e904c121
Hoscar <oscarodriguez.05.18@gmail.com>
2025-12-13 17:59:17 -0400

    game over and surrender

--- monsterSlayer/app.js
+++ monsterSlayer/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
+            winner: null
         }
     },
     methods: {
@@ -25,22 +26,63 @@ const app = Vue.createApp({
             const damageValue = getRandomValue(10,25)
             this.monsterHealth -= damageValue
             this.attackPlayer()
+        },
+        healPlayer() {
+            this.playerHealth += getRandomValue(15,8)
+        },
+        startNewGame() {
+            this.playerHealth = 100
+            this.monsterHealth = 100
+            this.currentRound = 0
+            this.winner = null
+        },
+        surrender() {
+            this.winner = 'monster'
         }
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return {
+                    width: '0%'
+                }
+            }
             return {
                 width: this.monsterHealth + '%'
             }
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return {
+                    width: '0%'
+                }
+            }
             return {
                 width: this.playerHealth + '%'
             }
         },
         specialAttackAvailable() {
             return this.currentRound % 3 !== 0
-        }
+        }, 
+        
+
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) { 
+                this.winner = 'draw'
+            } else if (value <= 0) { 
+                this.winner = 'monster'
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) { 
+                this.winner = 'draw'
+            } else if (value <= 0) { 
+                this.winner = 'player'
+            }
+        },
+
     }
 });
 
